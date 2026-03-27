687657652d5f3bef100f0a18a0d295957a5ee81b
pedroholiveira154 <pedro.h.oliveira154@aluno.senai.br>
2026-03-27 13:20:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function vercel() {
+    // Swal.fire({
+    //     title: "Поздравляем!!",
+    //     text: "Вы разместили его на сайте Vercel ))",
+    //     icon: "success"
+    // });
+    Swal.fire({
+        title: "Parabéns!",
+        text: "Você hospedou no site do Vercel :)",
+        icon: "success"
+    });
+}
